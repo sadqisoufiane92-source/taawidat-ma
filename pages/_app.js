309ce45c8be3585c6ea2695750834080b08989ca
sadqisoufiane92-source <sadqisoufiane92-source@users.noreply.github.com>
2026-05-01 13:00:33 +0100

    feat: add Google Analytics 4

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,29 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 
+const GA_ID = 'G-8QP3XESLWT'; // replace with your actual Measurement ID
+
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;600;700;800;900&family=Noto+Kufi+Arabic:wght@400;500;600;700;800;900&family=Noto+Naskh+Arabic:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;600;700;800;900&family=Noto+Kufi+Arabic:wght@400;500;600;700;800;900&family=Noto+Naskh+Arabic:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}');
+            `,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
